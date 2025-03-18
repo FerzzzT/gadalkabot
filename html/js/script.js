3253aadb644b9d085c2b239197916d5146680d63
Green_Tea <farm49595@gmail.com>
2025-03-18 16:34:26 +0300

    commit

---
+++ html/js/script.js
@@ -0,0 +1,56 @@
+
+alert("Mini App готово!")
+document.addEventListener("DOMContentLoaded", function () {
+    const cardImages = [
+        'css/cards/the_chariot.jpg', 'css/cards/the_fool.jpg', 'css/cards/the_magician.jpg',
+        'css/cards/the_high_priestess.jpg', 'css/cards/the_empress.jpg', 'css/cards/the_emperor.jpg',
+        'css/cards/the_hierophant.jpg', 'css/cards/the_star.jpg', 'css/cards/the_sun.jpg',
+        'css/cards/the_lover.jpg','css/cards/strength.jpg','css/cards/the_hermit.jpg',
+        'css/cards/justice.jpg','css/cards/wheel_fortune.jpg','css/cards/death.jpg',
+        'css/cards/the_hanged_man.jpg','css/cards/temperance.jpg','css/cards/the_world.jpg',
+        'css/cards/judgement.jpg','css/cards/the_moon.jpg','css/cards/the_tower.jpg',
+        'css/cards/the_devil.jpg','css/cards/age_pentacles.jpg','css/cards/2d.jpg',
+        'css/cards/3d.jpg','css/cards/4d.jpg','css/cards/5d.jpg',
+        'css/cards/9d.jpg','css/cards/10d.jpg','css/cards/8d.jpg',
+        'css/cards/king_pentacles.jpg','css/cards/6d.jpg','css/cards/7d.jpg',
+        'css/cards/queen_pentacles.jpg','css/cards/knight_pentacles.jpg','css/cards/page_pentacles.jpg',
+        'css/cards/age_swords.jpg','css/cards/2s.jpg','css/cards/3s.jpg',
+        'css/cards/4s.jpg','css/cards/5s.jpg','css/cards/6s.jpg',
+        'css/cards/7s.jpg','css/cards/8s.jpg','css/cards/9s.jpg',
+        'css/cards/10s.jpg','css/cards/ks.jpg','css/cards/ps.jpg',
+        'css/cards/king_swords.jpg','css/cards/qs.jpg',
+        'css/cards/1g.jpg','css/cards/2g.jpg','css/cards/3g.jpg',
+        'css/cards/4g.jpg','css/cards/5g.jpg','css/cards/6g.jpg',
+        'css/cards/7g.jpg','css/cards/8g.jpg','css/cards/9g.jpg',
+        'css/cards/10g.jpg','css/cards/qg.jpg','css/cards/kw.jpg',
+        'css/cards/pw.jpg','css/cards/king_g.jpg',
+    ];
+    const shuffledCards = cardImages.sort(() => 0.5 - Math.random()).slice(0, 9);
+    const cardsContainer = document.getElementById("cardsContainer");
+    let selectedCards = [];
+    let selectedCardNames = [];
+
+    shuffledCards.forEach((image) => {
+        const card = document.createElement("div");
+        card.classList.add("tarot-card", "cursor-pointer");
+        card.innerHTML = `<div class="card-back"></div><div class="card-placeholder" style="background-image: url('${image}')"></div>`;
+
+        card.addEventListener("click", function () {
+            if (!selectedCards.includes(this) && selectedCards.length < 3) {
+                selectedCards.push(this);
+                selectedCardNames.push(image);
+                this.classList.add("flipped", "selected");
+                this.classList.remove("cursor-pointer");
+            }
+            document.getElementById("continueBtn").classList.toggle("hidden", selectedCards.length !== 3);
+        });
+
+        cardsContainer.appendChild(card);
+    });
+
+
+    document.getElementById("sendData").addEventListener("click", function() {
+        let data = {cards: selectedCards};
+
+    })
+})
